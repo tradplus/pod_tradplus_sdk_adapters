6d62fbcd674a1450ddc7f18a7b142174fcf58861
xuejun <xuejun.pei@tradplus.com>
2022-03-17 21:03:44 +0800

    v1.0.0

---
+++ PodTrunk.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cd $(dirname $0)
+
+networkArray=("AdColony" "AdMob" "AppLovin" "Facebook" "GDTMob" "InMobi" "IronSource" "KuaiShou" "Maio" "Mintegral" "MoPub" "MyTarget" "Pangle" "Sigmob" "Smaato" "Tapjoy" "Unity" "Vungle" "YouDao" "StartApp" "Kidoz" "Klevin" "Chartboost" "TPCross" "Fyber" "SuperAwesome" "Baidu" "Ogury" "TPADX")
+
+for(( i=0;i<${#networkArray[@]};i++)) do
+
+networkName=${networkArray[i]}
+
+pod trunk push TradPlus${networkName}Adapter.podspec  --allow-warnings --skip-import-validation
+
+done;
